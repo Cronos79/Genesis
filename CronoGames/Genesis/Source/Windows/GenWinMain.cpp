36880744ecd6be05308c393130c0e5a5a3bfc51a
MattB79 <cronos1979@live.com>
2022-06-27 22:33:24 -0500

    Init vs

---
+++ CronoGames/Genesis/Source/Windows/GenWinMain.cpp
@@ -0,0 +1,39 @@
+/*
+* Author: Matt Brock
+* Copyright: 
+*/
+
+#include "windows.h"
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR cmd, int)
+{
+
+	const auto pClassName = "GenWindowClass";
+	// register window class
+	WNDCLASSEX wc = { 0 };
+	wc.cbSize = sizeof(wc);
+	wc.style = CS_OWNDC;
+	wc.lpfnWndProc = DefWindowProc;
+	wc.cbClsExtra = 0;
+	wc.cbWndExtra = 0;
+	wc.hInstance = hInstance;
+	wc.hIcon = nullptr;
+	wc.hCursor = nullptr;
+	wc.hbrBackground = nullptr;
+	wc.lpszMenuName = nullptr;
+	wc.lpszClassName = pClassName;
+	wc.hIconSm = nullptr;
+	RegisterClassEx(&wc);
+	// create window instance
+	HWND hWnd = CreateWindowEx(
+		0, pClassName,
+		"Genesis",
+		WS_CAPTION | WS_MINIMIZEBOX | WS_SYSMENU,
+		200, 200, 640, 480,
+		nullptr, nullptr, hInstance, nullptr
+	);
+	// show the damn window
+	ShowWindow(hWnd, SW_SHOW);
+	while (true);
+	return 0;
+}
